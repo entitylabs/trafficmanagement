cd1420cc7156dd73dc392ce6cd9c42e7b56d072d
entitylabs <50488610+entitylabs@users.noreply.github.com>
2021-08-03 23:54:55 +0200

    Delete src/traffic/management/scheduler directory

--- src/traffic/management/scheduler/observers/Observer.java
+++
@@ -1,8 +0,0 @@
-package traffic.management.scheduler.observers;
-
-import traffic.management.trafficlights.TrafficLight;
-
-public interface Observer {
-
-	public void update(TrafficLight trafficLight);
-}
